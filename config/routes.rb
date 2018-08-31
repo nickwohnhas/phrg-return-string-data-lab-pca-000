cd1ac654967394b4432ad009ccc1707491a9317e
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-31 14:45:01 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  get '/products/:id/inventory', to: 'products#inventory'
+  get '/products/:id/description', to: 'products#description'
+  # get ''
   resources :orders
   resources :invoices
+  resources :products
 end
